c232e5af02acd6a8de7288c20d308bc640c625b7
Jaydeep <jaydeeprathod16167@gmail.com>
2022-05-19 16:47:47 +0530

    Revert "Home Screen Created"
    This reverts commit ab554944

--- lib/HOmeScreen.dart
+++ lib/HOmeScreen.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
-
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -20,12 +18,6 @@ class _HomeScreenState extends State<HomeScreen> {
           Text('AAAA DEmo'),
           Text('AAAA DEmo'),
           Text('AAAA DEmo'),
-          Text('After Changes DEmo'),
-          Text('After Changes DEmo'),
-          Text('After Changes DEmo'),
-          Text('After Changes DEmo'),
-          Text('After Changes DEmo'),
-
         ],
       ),
     );
